d5b3db0bdb4718c0f4716c55f302ad745ce3f344
shaibimalik <shaibimalik4772@gmail.com>
2022-06-02 19:39:45 +0500

    Add project files.

---
+++ assesment/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using assesment.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assesment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var client = new RestClient("https://api.currencyapi.com");
+            var request = new RestRequest() {
+                 Method = Method.Get,
+                 Resource= "/v3/latest?apikey=IG5u9PqkFLugbHgRXo79eoGt4WUT8vcJ7RECLa8b&currencies=EUR%2CUSD%2CCAD"
+            };
+            var response = client.ExecuteGetAsync(request).Result;
+
+            var content = response.Content;
+            return Json(content);
+        }
+
+      
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ assesment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace assesment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
